4b1c7cb170b335a5648fe1e2b1e2bc6c27641f7d
tpgh02 <seaho4588@gmail.com>
2025-02-21 22:12:31 +0900

    프로그래밍 패러다임 - 미니퀘스트

---
+++ ProgrammingParadigm/Map.js
@@ -0,0 +1,7 @@
+var nums = [1, 2, 3, 4, 5];
+
+var mul = nums.map(function(value){
+    return value*2;
+});
+
+console.log(mul);
---
+++ ProgrammingParadigm/Reduce.js
@@ -0,0 +1,5 @@
+var nums = [1, 2, 3, 4, 5];
+
+const sum = nums.reduce((x, y) => x+y);
+
+console.log(sum)
