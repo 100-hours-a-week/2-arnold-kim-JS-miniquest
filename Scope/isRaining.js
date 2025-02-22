bc4e4ec7e16caa18dcf886616bca748dbabab74e
tpgh02 <seaho4588@gmail.com>
2025-02-22 14:05:35 +0900

    스코프 - 미니퀘스트

---
+++ Scope/isRaining.js
@@ -0,0 +1,8 @@
+if (true) {
+    let isRaining = true;
+}
+
+console.log(isRaining);
+
+// if문 안에서 let으로 선언된 isRaining은 블록 스코프로, 외부에서 접근할 수 없다.
+// 블록 스코프의 스코프제한을 벗어나고 싶다면 var 타입으로 선언하면 블록 외부에서도 사용할 수 있다.
---
+++ Scope/temperature.js
@@ -0,0 +1,7 @@
+let temperature = 25;
+temperature = 30;
+
+const MAX_TEMPERATURE = 35;
+// MAX_TEMPERATURE = 30; TypeError!
+
+// const는 불변하는 변수를 담는 자료형이다. 그렇기 때문에 const의 값을 바꾸려고 재할당하면 오류가 발생한다.
